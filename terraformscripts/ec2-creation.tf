9f3ad0f7b0a7ad87499394943fe35eaeb5f17db0
pavankumar <119112873+pavankumarnagandla@users.noreply.github.com>
2023-01-23 12:47:30 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,13 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+region = "ap-south-1"
 }
-
-resource "aws_instance" "AWSEC2Instance"{
-     count   = 1
-     ami = "ami-010d564ad44bd1912"
-     instance_type = "t2.micro"
-     security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
-     tags = {
-        Name = "tomcatservers"
-     }
+resource "aws_instance" "AWSInstance"{
+    ami = "ami-of9d9a251c1a44858"
+    instance_type = "t2.micro"
+    security_groups = ["launch-wizard-2"]
+    key_name = pavankumarnagandla
+    tags = {
+    Name = "tomcatservers" 
+    }
 }
 
